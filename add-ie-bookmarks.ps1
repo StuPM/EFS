f2d31c7c5bc8605d100ebcb424cbb02893758716
StuPM <stuartpmcgee@gmail.com>
2016-08-09 09:26:22 +0100

    Add files via upload

---
+++ add-ie-bookmarks.ps1
@@ -0,0 +1,32 @@
+﻿# 1 - Used to create DM 
+$IEFav = [Environment]::GetFolderPath('Favorites', 'None') 
+$Shell = New-Object -ComObject WScript.Shell
+$IEFav = Join-Path -Path $IEFav -ChildPath 'Links'
+$Name = 'DM'
+$FullPath = Join-Path -Path $IEFav -ChildPath "$($Name).url"
+$url = 'http://localhost:80/dm/dm.do'
+
+$shortcut = $Shell.CreateShortcut($FullPath)
+$shortcut.TargetPath = $url
+$shortcut.Save()
+
+# 2 - Used to create JFinder 
+$Name = 'JFinder'
+$FullPath = Join-Path -Path $IEFav -ChildPath "$($Name).url"
+$url = 'http://localhost:8080/jfinder'
+
+$shortcut = $Shell.CreateShortcut($FullPath)
+$shortcut.TargetPath = $url
+$shortcut.Save()
+
+# 3 - Used to create BPM 
+$Name = 'BPM'
+$FullPath = Join-Path -Path $IEFav -ChildPath "$($Name).url"
+$url = 'http://localhost:8180/bpm'
+
+$shortcut = $Shell.CreateShortcut($FullPath)
+$shortcut.TargetPath = $url
+$shortcut.Save()
+
+$Name = $MyInvocation.MyCommand.Name
+Write-Host $Name "done"
